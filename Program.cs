e7ee48a688ddd89d7452190caf42939a44f98742
tjhindman <thind131@gmail.com>
2022-08-30 18:29:07 -0400

    wrong process

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, noobs!");
 
+System.Console.WriteLine("Dont do this!!!");
+
 Console.WriteLine("Hello, from teammate account!");
