786a331e86cafb1feaf01be8662aa922d8ad8a0c
Thomas E Hindman, Jr <45403075+thomashindmanjr@users.noreply.github.com>
2022-08-30 18:27:43 -0400

    note to self

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, noobs!");
 
+// TJ Stop being so slow!
+
 Console.WriteLine("Hello, from teammate account!");
